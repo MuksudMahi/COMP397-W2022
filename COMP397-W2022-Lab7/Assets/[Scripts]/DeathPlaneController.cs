34c02584fc621cb41605c010aee2cc48a3fbbee8
Muksud Hussain Mahi <mmahi1@mycentennialcollege.ca>
2022-03-02 01:08:13 -0500

    Lab 7 added project files

---
+++ COMP397-W2022-Lab7/Assets/[Scripts]/DeathPlaneController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlaneController : MonoBehaviour
+{
+    public Transform playerReSpawn;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            ReSpawnPlayer(other.gameObject);
+        }
+    }
+
+    private void ReSpawnPlayer(GameObject player)
+    {
+        player.GetComponent<CharacterController>().enabled = false;
+        player.transform.position = playerReSpawn.position;
+        player.GetComponent<CharacterController>().enabled = true;
+
+    }
+}
